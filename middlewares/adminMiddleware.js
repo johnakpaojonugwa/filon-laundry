be514c84738313ac9d307076a5c8429c55d1008e
John Akpa Ojonugwa <johnakpaojonugwa@gmail.com>
2026-05-08 16:24:30 +0100

    edited the admin middleware

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,5 +1,5 @@
 export const adminOnly = (req, res, next) => {
-    if (!req.user || req.user.role !== 'SUPER_ADMIN') {
+    if (!req.user || req.user.role !== 'super_admin') {
         return res.status(403).json({
             success: false,
             message: 'Forbidden: Super admin only'
